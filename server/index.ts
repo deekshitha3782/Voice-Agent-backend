4ea888c7070dfbc9f897ba26c857b0b12d0d3c52
deekshitha3782 <deekkshitha02@gmail.com>
2026-01-28 19:15:55 +0530

    Remove config import from server/index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,4 @@
-import "./config";
+// dotenv not needed - Railway provides env vars directly
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { serveStatic } from "./static";
